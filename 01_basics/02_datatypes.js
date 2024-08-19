f8d32451efe0e9979e832b734fa88486ea2e0dcd
Ashish Kumar <118350243+ashish-code6@users.noreply.github.com>
2024-08-19 12:39:52 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+let namee='ashish';// string
+let empNo=214; // number
+let salary=false;
+
+let location; 
+let jobrole=null;
+console.table([namee,empNo,salary,location,jobrole]);
+
+console.log(typeof(jobrole));
+
+console.log(typeof(null));
+
+console.log(typeof(undefined));
