8e23501d1c2a68db4e499bbbc1b1602aad9804dc
Ashish Kumar <118350243+ashishpatna005@users.noreply.github.com>
2024-08-19 07:44:14 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const distance=25634;
+
+let name="ashish";
+var email="ashishk@123";
+empl_id=246578;
+
+// distance=15247;
+// it is not redecalre.
+name="rohan";
+email="rohan@321";
+empl_id=205;
+let location;
+//  it show undefined.
+
+console.log(distance);
+
+console.table([distance,name,email,empl_id,location]);
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,3 @@
-console.log("Ashish");
 console.log("Hii");
+console.log("Ashish");
+let="hello";
