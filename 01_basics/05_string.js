e485a3910ce708784aff276ab9b1fd75d46ecb87
Ashish Kumar <118350243+ashish-code6@users.noreply.github.com>
2024-08-20 10:26:58 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,25 @@
+const str1='This is Primitive string.';
+
+const str2=new String("This is Object string");
+
+// console.table([str1,str2]);
+
+ const sentance=" JavaScript is a dynamic Language."
+// const words='dynamic';
+
+// console.log(`Present at index ${index} is ${sentance.charAt(index)}`);--charAt()
+// console.log(`${words} charcter present at ${sentance.indexOf(words)}`);--indexof()
+
+// console.log(str1.concat(str2));---concatation
+const str = 'The quick brown fox jumps over the lazy dog.';
+// console.log(str.slice(-9, -5));
+
+// console.log(str1.slice(2,5));
+
+const mood='aggresive ';
+
+console.log(`my mood is ${mood.repeat(3)}`);
+
+
+
+
