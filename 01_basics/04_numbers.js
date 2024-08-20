fddab6b7121b8fb84ca194076fa0bf239d3560d8
Ashish Kumar <118350243+ashish-code6@users.noreply.github.com>
2024-08-20 11:05:22 +0000

    number

---
+++ 01_basics/04_numbers.js
@@ -0,0 +1,7 @@
+const numbers=100000;
+// console.log(numbers.toFixed(2));
+// console.log(numbers.toLocaleString('en-IN'));
+
+const ones=Number(1000);
+
+console.table([typeof(numbers.toString()),typeof(ones)])
