5057a8aa212f67b877b829555fa10bc37500c888
Ashish Kumar <118350243+ashish-code6@users.noreply.github.com>
2024-08-21 17:24:14 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,14 @@
+// let myarr=[1,2,4,7,5,6];
+
+// myarr.push(6);// add the elemnt at last index.
+// myarr.pop();// delete the element at last index.
+
+// // console.log(myarr.slice(1,3)); slice is used to slicing the array form start index to end index.
+
+// // myarr.splice(1,3); splice is also used for sliceing the array but it change in original array. 
+
+// // myarr.unshift(9); unshipt method is used add the starting index from array.
+
+// // myarr.shift() shif method is used to delete the element from start of the  array 
+
+// console.log(myarr);
